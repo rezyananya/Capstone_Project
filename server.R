29171802047179eb66d2219f9bdf0c94d091a4ab
rezyananya <31268006+rezyananya@users.noreply.github.com>
2017-12-25 22:21:02 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,31 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(NLP)
+library(tm)
+library(RWeka)
+#library(rsconnect)
+#library(data.table)
+#library(dplyr)
+#library(wordcloud)
+source("C:/Users/Priyanka/Documents/Capstone_Predict_Word/myapp/NLP_Programe.R")
+
+
+shinyServer(function(input, output) {
+  output$inputValue <- renderPrint({input$Tcir})
+  output$prediction <- renderPrint({Wordform(input$Tcir)})
+  
+  
+  #output$Plot <- renderPlot({
+  #wordcloud(Corpus_form, max.words = 100, random.order = FALSE,rot.per=0.45, 
+  #  use.r.layout=FALSE,colors=brewer.pal(8, "Dark2"))}
+  #)
+  
+})
